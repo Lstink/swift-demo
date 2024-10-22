10621358011b7181c06a79228fe884e6d7f442c8
lstink <1318385889@qq.com>
2024-10-22 11:03:26 +0800

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by lstink on 2024/10/22.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
